01e8c5f56aa0a92a05de5cd9b170c1cac12dc4c7
Carlos Cunha <carlosrubim10@gmail.com>
2025-03-10 12:20:17 -0300

    Tratamento de erros

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,19 +1,38 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { log } from 'node:console';
 
 @Controller('tasks')
 export class TasksController {
 
-  constructor(private readonly taskService: TasksService){}
+  constructor(private readonly taskService: TasksService) { }
 
-    @Get()
-    findAllTasks() {
-      return this.taskService.findAll()
-    }
+  @Get()
+  findAllTasks() {
+    return this.taskService.findAll()
+  }
 
-    @Get(":id")
-    findOneTask(@Param('id') id: string){
-      return this.taskService.findOne(id)
-    }
+  @Get(":id")
+  findOneTask(@Param('id') id: string) {
+    return this.taskService.findOne(id)
+  }
+
+  @Post()
+  createTask(@Body() body: any) {
+    console.log(body);
+    return this.taskService.create(body)
+
+  }
+
+  @Patch(":id")
+  updateTask(@Param("id") id: string, @Body() body: any) {
+    return this.taskService.update(id, body)
+  }
+
+  @Delete(":id")
+  deleteTask(@Param("id") id: string) {
+    console.log("ID ENVIADO: " + id);
+    return "Deletar a tarefa com id " + id
+
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,15 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { Task } from './entities/task.entity';
 
 @Injectable()
 export class TasksService {
 
-  findAll(){
-    return [
-      { id: 1, task:"Comprar Pão"}
-    ]
+  private tasks: Task[] = [
+    {
+      id: 1,
+      name: "Seguir o sujeito programador no YouTube",
+      description: "Aprendendo muito sobre programação",
+      completed: false
+    }
+  ]
+
+  findAll() {
+    return this.tasks;
   }
 
-  findOne(id: string){
-    return "Buscar tarefa com ID: " + id
+  findOne(id: string) {
+    const task = this.tasks.find(task => task.id === Number(id))
+
+    if (task) return task;
+
+    throw new HttpException("Essa tarefa não existe! ", HttpStatus.NOT_FOUND)
+    //throw new NotFoundException("Essa tarefa não existe")
+  }
+
+  create(body: any) {
+    const newId = this.tasks.length + 1;
+    const newTask = {
+      id: newId,
+      ...body,
+    }
+
+    this.tasks.push(newTask)
+    return newTask
+  }
+
+  update(id: string, body: any) {
+    const taskIndex = this.tasks.findIndex(task => task.id == Number(id))
+
+    if (taskIndex < 0) {
+      throw new HttpException("Essa tarefa não existe! ", HttpStatus.NOT_FOUND)
+    }
+    const taskItem = this.tasks[taskIndex]
+
+    this.tasks[taskIndex] = {
+      ...taskItem,
+      ...body,
+    }
+    return this.tasks[taskIndex]
   }
 }
